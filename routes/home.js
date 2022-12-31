b3a04bb3f0d12f80dac74b5c67574251287b7077
Edward Stalley <83490268+Edward-Stalley@users.noreply.github.com>
2022-12-31 18:10:53 +0900

    Update

--- routes/home.js
+++ routes/home.js
@@ -1,23 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const mysql = require("mysql");
-
-const db = mysql.createConnection({
-  host: "sql6.freemysqlhosting.net",
-  user: "sql6587358",
-  password: "beh4prkep6",
-  database: "sql6587358",
-});
 
 router.get("/", async (req, res, next) => {
-  const q = `SELECT * FROM listening_kiso`;
-  db.query(q, (err, data) => {
-    if (err) return res.json(err);
-    // res.set("Access-Control-Allow-Origin", "*");
-    // res.send({ msg: "This has CORS enabled 🎈" });
-    return res.status(200).json(data);
-  });
-
   return res.status(200).json({
     title: "Express Testing",
     message: "The app is working properly I hope I can add cards to it!",
